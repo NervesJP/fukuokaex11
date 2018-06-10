04a0a3d8e33a0229869d2e71a7e8a739ba1cd7cb
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-10 17:15:39 +0900

    Copy projects from [adkron/grovepi](https://github.com/adkron/grovepi)

---
+++ examples_on_grovepi/home_weather_display/lib/home_weather_display.ex
@@ -0,0 +1,54 @@
+defmodule HomeWeatherDisplay do
+  @moduledoc false
+  use GenServer
+  require Logger
+
+  defstruct [:dht]
+
+  alias GrovePi.{RGBLCD, DHT}
+
+  def start_link(pin) do
+    GenServer.start_link(__MODULE__, pin)
+  end
+
+  def init(dht_pin) do
+    state = %HomeWeatherDisplay{dht: dht_pin}
+
+    RGBLCD.initialize()
+    RGBLCD.set_text("Ready!")
+
+    DHT.subscribe(dht_pin, :changed)
+    {:ok, state}
+  end
+
+  def handle_info({_pin, :changed, %{temp: temp, humidity: humidity}}, state) do
+    temp = format_temp(temp)
+    humidity = format_humidity(humidity)
+
+    flash_rgb()
+
+    RGBLCD.set_text(temp)
+    RGBLCD.set_cursor(1, 0)
+    RGBLCD.write_text(humidity)
+    Logger.info temp <> " " <> humidity
+    {:noreply, state}
+  end
+
+  def handle_info(_message, state) do
+    {:noreply, state}
+  end
+
+  defp flash_rgb() do
+    RGBLCD.set_rgb(255, 0, 0)
+    Process.sleep(1000)
+    RGBLCD.set_color_white()
+  end
+
+  defp format_temp(temp) do
+    "Temp: #{Float.to_string(temp)} C"
+  end
+
+  defp format_humidity(humidity) do
+    "Humidity: #{Float.to_string(humidity)}%"
+  end
+end
