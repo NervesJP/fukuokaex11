c0209d470b817cc14b5cdae45e1bbaa0301fc7b7
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 00:18:10 +0900

    Mod mix.exs

--- dht_sensor/lib/dht_sensor.ex
+++ dht_sensor/lib/dht_sensor.ex
@@ -1,18 +1,64 @@
 defmodule DhtSensor do
-  @moduledoc """
-  Documentation for DhtSensor.
-  """
+  @moduledoc false
+  use Application
 
-  @doc """
-  Hello world.
+  # Port on GrovePi+
+  @dht_pin 7 # Use port 7 for the DHT
+  @dht_poll_interval 1_000 # poll every 1 second
 
-  ## Examples
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
 
-      iex> DhtSensor.hello
-      :world
+    children = [
+      # Start the GrovePi sensor we want
+      worker(GrovePi.DHT, [@dht_pin, [poll_interval: @dht_poll_interval]]),
 
-  """
-  def hello do
-    :world
+      # Start the main app
+      worker(DhtSensor.Worker, [@dht_pin]),
+    ]
+
+    opts = [strategy: :rest_for_one, name: DhtSensor.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defmodule Worker do
+    @moduledoc false
+    use GenServer
+
+    defstruct [:dht]
+
+    def start_link(pins) do
+      GenServer.start_link(__MODULE__, pins)
+    end
+
+    def init(dht_pin) do
+      state = %Worker{dht: dht_pin}
+
+      GrovePi.Dht.subscribe(dht_pin, :changed)
+      {:ok, state}
+    end
+
+    def handle_info({_pin, :changed, %{temp: temp, humidity: humidity}}, state) do
+      temp = format_temp(temp)
+      humidity = format_humidity(humidity)
+
+      IO.puts temp
+      IO.puts humidity
+
+      {:noreply, state}
+    end
+
+    def handle_info(_message, state) do
+      {:noreply, state}
+    end
+
+    defp format_temp(temp) do
+      "Temp: #{Float.to_string(temp)} C"
+    end
+
+    defp format_humidity(humidity) do
+      "Humidity: #{Float.to_string(humidity)}%"
+    end
+
   end
 end
