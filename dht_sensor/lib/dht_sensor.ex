8571b689e4441ec00bbafafc8e76893e632daf82
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 00:02:09 +0900

    mix new dht_sensor

---
+++ dht_sensor/lib/dht_sensor.ex
@@ -0,0 +1,18 @@
+defmodule DhtSensor do
+  @moduledoc """
+  Documentation for DhtSensor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DhtSensor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ dht_sensor/test/dht_sensor_test.exs
@@ -0,0 +1,8 @@
+defmodule DhtSensorTest do
+  use ExUnit.Case
+  doctest DhtSensor
+
+  test "greets the world" do
+    assert DhtSensor.hello() == :world
+  end
+end
