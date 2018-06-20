9fc252ae18b24386f406d6764677e7652728c847
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 00:45:08 +0900

    mod source

--- home_weather_display_host/lib/home_weather_display_host.ex
+++ home_weather_display_host/lib/home_weather_display_host.ex
@@ -1,18 +1,81 @@
 defmodule HomeWeatherDisplayHost do
-  @moduledoc """
-  Documentation for HomeWeatherDisplayHost.
-  """
+  @moduledoc false
+  use Application
 
-  @doc """
-  Hello world.
+  # Port on GrovePi+
+  @dht_pin 7 # Use port 7 for the DHT
+  @dht_poll_interval 1_000 # poll every 1 second
 
-  ## Examples
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
 
-      iex> HomeWeatherDisplayHost.hello
-      :world
+    children = [
+      # Start the GrovePi sensor we want
+      worker(GrovePi.DHT, [@dht_pin, [poll_interval: @dht_poll_interval]]),
 
-  """
-  def hello do
-    :world
+      # Start the main app
+      worker(HomeWeatherDisplayHost.Worker, [@dht_pin]),
+    ]
+
+    opts = [strategy: :rest_for_one, name: HomeWeatherDisplay.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defmodule Worker do
+    @moduledoc false
+    use GenServer
+    require Logger
+
+    defstruct [:dht]
+
+    def start_link(pins) do
+      GenServer.start_link(__MODULE__, pins)
+    end
+
+    def init(dht_pin) do
+      state = %Worker{dht: dht_pin}
+
+      RGBLCD.initialize()
+      RGBLCD.set_text("Ready!")
+
+      GrovePi.DHT.subscribe(dht_pin, :changed)
+      {:ok, state}
+    end
+
+    def handle_info({_pin, :changed, %{temp: temp, humidity: humidity}}, state) do
+      temp = format_temp(temp)
+      humidity = format_humidity(humidity)
+
+      #IO.puts temp
+      #IO.puts humidity
+
+      flash_rgb()
+
+      RGBLCD.set_text(temp)
+      RGBLCD.set_cursor(1, 0)
+      RGBLCD.write_text(humidity)
+      Logger.info temp <> " " <> humidity
+
+      {:noreply, state}
+    end
+
+    def handle_info(_message, state) do
+      {:noreply, state}
+    end
+
+    defp flash_rgb() do
+      RGBLCD.set_rgb(255, 0, 0)
+      Process.sleep(1000)
+      RGBLCD.set_color_white()
+    end
+
+    defp format_temp(temp) do
+      "Temp: #{Float.to_string(temp)} C"
+    end
+
+    defp format_humidity(humidity) do
+      "Humidity: #{Float.to_string(humidity)}%"
+    end
+
   end
 end
--- home_weather_display_host/mix.exs
+++ home_weather_display_host/mix.exs
@@ -6,6 +6,7 @@ defmodule HomeWeatherDisplayHost.MixProject do
       app: :home_weather_display_host,
       version: "0.1.0",
       elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,15 +15,15 @@ defmodule HomeWeatherDisplayHost.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {HomeWeatherDisplayHost, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:grovepi, github: "adkron/grovepi", branch: "master"}
     ]
   end
 end
