9c767123fded57e548efeac7e06d2be31aa706e9
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 14:57:57 +0900

    Change interval

--- home_weather_display_host/lib/home_weather_display_host/application.ex
+++ home_weather_display_host/lib/home_weather_display_host/application.ex
@@ -4,7 +4,7 @@ defmodule HomeWeatherDisplayHost.Application do
 
   # RGB LCD Screen should use the IC2-1 port
   @dht_pin 7 # Use port 7 for the DHT
-  @dht_poll_interval 1_000 # poll every 1 second
+  @dht_poll_interval 3_000 # poll every 3 second
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
