4ba988bb1cafc07906e190201e743ec8731e595f
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 19:42:07 +0900

    mod source

--- phx_csvchart/config/prod.exs
+++ phx_csvchart/config/prod.exs
@@ -7,6 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :phx_csvchart, PhxCsvchartWeb.Endpoint,
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: false,
   check_origin: false,
