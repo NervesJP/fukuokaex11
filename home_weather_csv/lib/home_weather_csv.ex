5be0f001060a1fc8552d2795830c503c0919065e
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 14:29:27 +0900

    Write date to csv

--- home_weather_csv/lib/home_weather_csv.ex
+++ home_weather_csv/lib/home_weather_csv.ex
@@ -26,7 +26,9 @@ defmodule HomeWeatherDisplayCsv do
 
   def handle_info({_pin, :changed, %{temp: temp, humidity: humidity}}, state) do
     # Get date
-    date = DateTime.utc_now() |> DateTime.to_string()
+    #date = DateTime.utc_now() |> DateTime.to_string()
+    date = Timex.now("Asia/Tokyo")
+      |> Timex.parse( "%Y/%_m/%_d %_H:%_M:%_S", :strftime )
     # Write data to CSV
     File.write "dhtdata.csv", "#{date},#{temp},#{humidity}\n", [:append]
 
