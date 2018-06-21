6654d777e0a90a26aaa301b4f246df47950b5b91
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 20:17:54 +0900

    modify source

--- home_weather_phx/lib/home_weather_phx.ex
+++ home_weather_phx/lib/home_weather_phx.ex
@@ -1,9 +1,67 @@
 defmodule HomeWeatherPhx do
-  @moduledoc """
-  HomeWeatherPhx keeps the contexts that define your domain
-  and business logic.
+  @moduledoc false
+  use GenServer
+  use Timex
+  require Logger
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defstruct [:dht]
+
+  alias GrovePi.{RGBLCD, DHT}
+
+  def start_link(pin) do
+    GenServer.start_link(__MODULE__, pin)
+  end
+
+  def init(dht_pin) do
+    state = %HomeWeatherPhx{dht: dht_pin}
+
+    RGBLCD.initialize()
+    RGBLCD.set_text("Ready!")
+
+    # Create CSV file
+    File.write "priv/static/dhtdata.csv", ""
+
+    DHT.subscribe(dht_pin, :changed)
+    {:ok, state}
+  end
+
+  def handle_info({_pin, :changed, %{temp: temp, humidity: humidity}}, state) do
+    # Get date
+    #date = DateTime.utc_now() |> DateTime.to_string()
+    date = Timex.now("Asia/Tokyo")
+      |> Timex.format!( "%Y-%m-%d %H:%M:%S", :strftime )
+
+    # Write data to CSV
+    File.write "priv/static/dhtdata.csv", "#{date},#{temp},#{humidity}\n", [:append]
+
+    temp = format_temp(temp)
+    humidity = format_humidity(humidity)
+
+    flash_rgb()
+
+    RGBLCD.set_text(temp)
+    RGBLCD.set_cursor(1, 0)
+    RGBLCD.write_text(humidity)
+    Logger.info temp <> " " <> humidity
+
+    {:noreply, state}
+  end
+
+  def handle_info(_message, state) do
+    {:noreply, state}
+  end
+
+  defp flash_rgb() do
+    RGBLCD.set_rgb(255, 0, 0)
+    Process.sleep(1000)
+    RGBLCD.set_color_white()
+  end
+
+  defp format_temp(temp) do
+    "Temp: #{Float.to_string(temp)} C"
+  end
+
+  defp format_humidity(humidity) do
+    "Humidity: #{Float.to_string(humidity)}%"
+  end
 end
--- home_weather_phx/lib/home_weather_phx/application.ex
+++ home_weather_phx/lib/home_weather_phx/application.ex
@@ -1,10 +1,14 @@
 defmodule HomeWeatherPhx.Application do
   use Application
 
+  # RGB LCD Screen should use the IC2-1 port
+  @dht_pin 7 # Use port 7 for the DHT
+  @dht_poll_interval 3_000 # poll every 3 second
+
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
+    import Supervisor.Spec, warn: false
 
     # Define workers and child supervisors to be supervised
     children = [
@@ -12,6 +16,12 @@ defmodule HomeWeatherPhx.Application do
       supervisor(HomeWeatherPhxWeb.Endpoint, []),
       # Start your own worker by calling: HomeWeatherPhx.Worker.start_link(arg1, arg2, arg3)
       # worker(HomeWeatherPhx.Worker, [arg1, arg2, arg3]),
+
+      # Start the GrovePi sensor we want
+      worker(GrovePi.DHT, [@dht_pin, [poll_interval: @dht_poll_interval]]),
+
+      # Start the main app
+      worker(HomeWeatherDisplayCsv, [@dht_pin]),
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
