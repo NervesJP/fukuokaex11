9974d6d55e43153cf9da222f828dfdc97f50ca89
takasehideki <takase@i.kyoto-u.ac.jp>
2018-06-21 13:57:28 +0900

    Copy from _host

--- home_weather_csv/lib/home_weather_csv.ex
+++ home_weather_csv/lib/home_weather_csv.ex
@@ -1,18 +1,56 @@
-defmodule HomeWeatherCsv do
-  @moduledoc """
-  Documentation for HomeWeatherCsv.
-  """
+defmodule HomeWeatherDisplayCsv do
+  @moduledoc false
+  use GenServer
+  require Logger
 
-  @doc """
-  Hello world.
+  defstruct [:dht]
 
-  ## Examples
+  alias GrovePi.{RGBLCD, DHT}
 
-      iex> HomeWeatherCsv.hello
-      :world
+  def start_link(pin) do
+    GenServer.start_link(__MODULE__, pin)
+  end
 
-  """
-  def hello do
-    :world
+  def init(dht_pin) do
+    state = %HomeWeatherDisplayCsv{dht: dht_pin}
+
+    RGBLCD.initialize()
+    RGBLCD.set_text("Ready!")
+
+    DHT.subscribe(dht_pin, :changed)
+    {:ok, state}
+  end
+
+  def handle_info({_pin, :changed, %{temp: temp, humidity: humidity}}, state) do
+    temp = format_temp(temp)
+    humidity = format_humidity(humidity)
+
+    flash_rgb()
+
+    RGBLCD.set_text(temp)
+    RGBLCD.set_cursor(1, 0)
+    RGBLCD.write_text(humidity)
+    Logger.info temp <> " " <> humidity
+    {:noreply, state}
+  end
+
+  def handle_info(_message, state) do
+    {:noreply, state}
+  end
+
+  defp flash_rgb() do
+    RGBLCD.set_rgb(255, 0, 0)
+    Process.sleep(1000)
+    RGBLCD.set_color_white()
+  end
+
+  defp format_temp(temp) do
+    "Temp: #{Float.to_string(temp)} C"
+  end
+
+  defp format_humidity(humidity) do
+    "Humidity: #{Float.to_string(humidity)}%"
   end
 end
+
+
